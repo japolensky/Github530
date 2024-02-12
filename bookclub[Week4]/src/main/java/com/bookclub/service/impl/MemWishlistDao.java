b4f26f555b65844bf6e350973a3f8f30517fd6f6
Jason <japolensky@my365.bellevue.edu>
2024-02-11 21:31:13 -0600

    update for week4  json 2.1.0->2.8.0 - trying to get past commit complaint related to https://devhub.checkmarx.com/cve-details/CVE-2023-51074/
    Week4 is now complete, there is a checkmarks.com issue for the above issue.  Looking to resolve.  Version does not seem to matter, I think I need to comprehend how to have the environment adapt the version for json components at compilation.

---
+++ bookclub[Week4]/src/main/java/com/bookclub/service/impl/MemWishlistDao.java
@@ -0,0 +1,33 @@
+package com.bookclub.service.impl;
+
+import com.bookclub.model.WishlistItem;
+import com.bookclub.service.dao.WishListDao;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MemWishlistDao implements WishListDao {
+    List<WishlistItem> wishlist;
+
+    //this segment from Mr. Krasso
+    public MemWishlistDao() {
+        this.wishlist = new ArrayList<WishlistItem>();
+        this.wishlist.add(new WishlistItem("9780345339683", "The Hobbit or There and Back Again"));
+        this.wishlist.add(new WishlistItem("9780261103573", "The Fellowship of the Ring"));
+        this.wishlist.add(new WishlistItem("9780261102361", "The Two Towers"));
+        this.wishlist.add(new WishlistItem("9780261102378", "The Return of the King"));
+    }
+
+    @Override
+    public List<WishlistItem> list() { return this.wishlist;    }
+
+    @Override
+    public WishlistItem find(String key) {
+        for (WishlistItem item : wishlist) {
+            if (item.getIsbn().equals(key)) {
+                return item;
+            }
+        }
+        return new WishlistItem();
+    }}
+
