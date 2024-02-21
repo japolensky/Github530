593821760fc65b5e82e06b6891ee6ac6dd6e8c81
Jason <japolensky@my365.bellevue.edu>
2024-02-20 22:10:04 -0600

    Update for week 5 - in progress of getting mongo DB connection to work properly.
    cleared some of the version issues that the codecheck complains about.  I'm not sure if I have a clear handle on how to use MAVEN and dependencies properly.

---
+++ bookclub[Week5]/src/main/java/com/bookclub/service/dao/WishListDao.java
@@ -0,0 +1,7 @@
+package com.bookclub.service.dao;
+
+import com.bookclub.model.WishlistItem;
+import com.bookclub.service.GenericDao;
+
+public interface WishListDao extends GenericDao<WishlistItem, String> {
+}
