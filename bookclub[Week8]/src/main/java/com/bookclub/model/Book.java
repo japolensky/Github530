8abea65da02a805cea9e9c106fb8bfcaeccf0676
Jason <japolensky@my365.bellevue.edu>
2024-02-27 21:58:09 -0600

    w8 initial commit c - working already MR. Gold, get the application.properties from me or use from the zipfile of the project from week 6,7, or 8 - for oauth2 signon

--- bookclub[Week8]/src/main/java/com/bookclub/model/Book.java
+++ bookclub[Week8]/src/main/java/com/bookclub/model/Book.java
@@ -1,65 +1,70 @@
 package com.bookclub.model;
 
-import java.util.List;
-
 public class Book {
     private String isbn;
     private String title;
     private String description;
+    private String infoUrl;
     private int numOfPages;
-    private List<String> authors;
 
     public Book() {}
 
-    public Book(String isbn, String title, String description, int numOfPages, List<String> authors) {
+       public Book(String isbn, String title, String description, String infoUrl , int numOfPages) {
         this.isbn = isbn;
         this.title = title;
         this.description = description;
+        this.infoUrl = infoUrl;
         this.numOfPages = numOfPages;
-        this.authors = authors; }
-    public void setIsbn(String isbn) {
+         }
+    public Book(String isbn, String title, String infoUrl) {
         this.isbn = isbn;
+        this.title = title;
+        this.infoUrl = infoUrl;
     }
 
     public String getIsbn() {
         return isbn;
     }
 
-    public void setTitle(String title) {
-        this.title = title;
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
     }
 
     public String getTitle() {
         return title;
     }
 
-    public void setDescription(String description) {
-        this.description = description;
+    public void setTitle(String title) {
+        this.title = title;
     }
 
     public String getDescription() {
         return description;
     }
 
-    public void setNumOfPages(int numOfPages) {
-        this.numOfPages = numOfPages;
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getInfoUrl() {
+        return infoUrl;
+    }
+
+    public void setInfoUrl(String infoUrl) {
+        this.infoUrl = infoUrl;
     }
 
     public int getNumOfPages() {
         return numOfPages;
     }
 
-    public void setAuthors(List<String> authors) {
-        this.authors = authors;
-    }
-
-    public List<String> getAuthors() {
-        return authors;
+    public void setNumOfPages(int numOfPages) {
+        this.numOfPages = numOfPages;
     }
 
     @Override
     public String toString() {
-        return String.format("Book{isbn=%s, title=%s, description=%s, numOfPages=%s, authors=%s}", isbn, title, description, numOfPages, authors);
+        return String.format("Book{isbn=%s, title=%s, description=%s, numOfPages=%s, authors=%s}", isbn, title, description, infoUrl, numOfPages);
     }
 
 }
--- bookclub[Week8]/src/main/java/com/bookclub/web/HomeController.java
+++ bookclub[Week8]/src/main/java/com/bookclub/web/HomeController.java
@@ -1,7 +1,7 @@
 package com.bookclub.web;
 
 import com.bookclub.model.Book;
-import com.bookclub.service.impl.MemBookDao;
+import com.bookclub.service.impl.RestBookDao;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,7 +15,7 @@ import java.util.List;
     public class HomeController {
         @RequestMapping(method = RequestMethod.GET)
         public String showHome(Model model) {
-            MemBookDao bookDao = new MemBookDao();
+            RestBookDao bookDao = new RestBookDao();
             List<Book> books = bookDao.list();
 
             for (Book book : books) {
@@ -32,7 +32,7 @@ import java.util.List;
             String isbn = id;
             System.out.println(id);
 
-            MemBookDao bookDao = new MemBookDao();
+            RestBookDao bookDao = new RestBookDao();
             Book book = bookDao.find(isbn);
 
             System.out.println(book.toString());
