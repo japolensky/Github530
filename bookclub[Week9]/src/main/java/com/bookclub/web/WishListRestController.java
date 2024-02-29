a7d95d7f82f8485f471a7595574fdda35c798d1a
Jason <japolensky@my365.bellevue.edu>
2024-02-29 00:47:30 -0600

    w9 initial commit c

---
+++ bookclub[Week9]/src/main/java/com/bookclub/web/WishListRestController.java
@@ -0,0 +1,31 @@
+package com.bookclub.web;
+
+import com.bookclub.model.WishlistItem;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import com.bookclub.service.impl.MongoWishListDao;
+import com.bookclub.service.dao.WishListDao;
+
+
+import java.util.List;
+@RestController
+@RequestMapping(path = "/api/wishlist", produces = "application/json")
+@CrossOrigin(origins = "*")
+public class WishListRestController {
+    WishListDao wishlistDao = new MongoWishListDao();
+
+    @Autowired
+    private void setWishlistDao(WishListDao wishlistDao) {
+        this.wishlistDao = wishlistDao;
+    }
+
+    @RequestMapping(method = RequestMethod.GET)
+    public List<WishlistItem> showWishlist() {
+        return wishlistDao.list();
+    }
+
+    @RequestMapping(path = "/{id}", method = RequestMethod.GET)
+    public WishlistItem findById(@PathVariable String id) {
+        return wishlistDao.find(id);
+    }}
